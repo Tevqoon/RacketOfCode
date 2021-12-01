7bab60d9a0bafd09d876fcc58cc8f03661aaa9ce
Tevqoon <jure.smolar@gmail.com>
2021-12-01 17:56:51 +0100

    Additional

--- common.rkt
+++ common.rkt
@@ -1,29 +1,33 @@
 #lang racket
 
 (define (open-day n)
-  (let* [(day-str (~v n))
-         (day-filename (string-append "day_" day-str ".in"))]
+  "Opens the n-th day's input file as a list.
+   If it doesn't exist, pull it with the aoc package."
+  (let [(day-filename (format "day_~a.in" n))]
     (unless (file-exists? day-filename)
-      (system (string-append "raco aoc -d " day-str " > " day-filename)))
+      (system (format "raco aoc -d ~a > ~a" n day-filename)))
     (file->list day-filename)))
 
-(define (unpack-lists lsts)
+(define (unpack-lists lists)
   "Takes a list of lists and returns a list of their cars and a list of their cdrs.
    If any list is empty, return nil."
-  (define (aux lsts cars cdrs)
-    (cond ((empty? lsts) (cons (reverse cars) (reverse cdrs)))
-          ((empty? (car lsts)) '())
-          (#t (aux (cdr lsts) (cons (caar lsts) cars) (cons (cdar lsts) cdrs)))))
-  (aux lsts '() '()))
+  (define (aux lists cars cdrs)
+    (cond [(empty? lists) (cons (reverse cars) (reverse cdrs))]
+          [(empty? (car lists)) '()]
+          [#t (aux (cdr lists)
+                   (cons (caar lists) cars)
+                   (cons (cdar lists) cdrs))]))
+  (aux lists '() '()))
 
-(define (mapb proc . lsts)
-  "Map proc over lsts, returning when any one of them runs out."
-  (define (aux lsts acc)
-    (let [(unpacked (unpack-lists lsts))]
+(define (mapcar proc . lists)
+  "Map proc over lists, returning when any one of them runs out."
+  (define (aux lists acc)
+    (let [(unpacked (unpack-lists lists))]
       (if (empty? unpacked)
           (reverse acc)
-          (aux (cdr unpacked) (cons (apply proc (car unpacked))
-                                    acc)))))
-  (aux lsts '()))
+          (aux (cdr unpacked)
+               (cons (apply proc (car unpacked))
+                     acc)))))
+  (aux lists '()))
 
 (provide (all-defined-out))
--- day_1.rkt
+++ day_1.rkt
@@ -1,14 +1,16 @@
 #lang racket
 
 (require "common.rkt")
+
 (define input (open-day 1))
 
 (define (solver1 input)
-  (count identity (mapb < input (cdr input))))
+  (count identity (mapcar < input (cdr input))))
 
 (solver1 input)
 
 (define (solver2 input)
-  (solver1 (mapb + input (cdr input) (cddr input))))
+  (solver1 (mapcar + input (cdr input) (cddr input))))
 
 (solver2 input)
+ 
