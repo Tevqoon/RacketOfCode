95799793af25bb63a3f82ab499b12ee7363a9070
Tevqoon <jure.smolar@gmail.com>
2021-12-02 15:42:34 +0100

    Completely yassified day 2

--- common.rkt
+++ common.rkt
@@ -1,5 +1,9 @@
 #lang racket
 
+(require srfi/26) ; Cut macro for partial function application
+
+;; Administration
+
 (define (open-day n)
   "Opens the n-th day's input file as a list.
    If it doesn't exist, pull it with the aoc package."
@@ -13,6 +17,8 @@
       (system (format "raco aoc -a ~a ~a" n val))
       val))
 
+;; Mapping
+
 (define (mapcar f . xss)
   "A mufch better mapcar stolen from Muf."
   (define (aux acc . xss)
@@ -25,7 +31,7 @@
   "Literally just reduce smfh."
   (and (not (empty? xs)) (foldl f (first xs) (rest xs))))
 
-(define (pair-up lst n)
+(define (slice-up lst n)
   "Slices up a list into sublists length n. Excess elements ignored."
   (define (aux lst acc)
     (if (< (length lst) n)
@@ -33,14 +39,34 @@
         (aux (drop lst n) (cons (take lst n) acc))))
   (aux lst '()))
 
-;; Tuples are of form (x1 x2 . x3) etc. List with last el non-nil.
+(define (pair-up lst)
+  (slice-up lst 2))
+
+;; Tuples are of form (x1 x2 . x3).
+
+(define (tuple. proc . tuples)
+  "Applies proc to tuples elementwise.
+   Assumes same length tuples and at least one given element."
+  (define (aux tuples)
+    (if (pair? (car tuples))
+        (cons (apply proc (map car tuples))
+              (aux (map cdr tuples)))
+        (apply proc tuples)))
+  (aux tuples))
+
+;; Some basic tuple operations
 
 (define (+. . tuples)
-  "Tuple addition. Assumes at least one given element."
-  (if (pair? (car tuples))
-      (cons (apply +  (map car tuples))
-            (apply +. (map cdr tuples)))
-      (apply + tuples)))
+  (apply tuple. + tuples))
+
+(define (-. . tuples)
+  (apply tuple. - tuples))
+
+(define (*. . tuples)
+  (apply tuple. * tuples))
+
+(define (/. . tuples)
+  (apply tuple. / tuples))
 
 (define (mult2 tuple)
   "Multiply first two elements of a tuple"
@@ -48,4 +74,12 @@
       (* (car tuple) (cadr tuple))
       (* (car tuple) (cdr tuple))))
 
+(define (tapply procs . tuples)
+  "Applies procs to tuples componentwise. Assumes operations and vectors are all the same length"
+  (if (pair? procs)
+      (cons (apply (eval (car procs)) (map car tuples))
+            (tapply (cdr procs) (map cdr tuples)))
+      (apply (eval procs) (car tuples))))
+
 (provide (all-defined-out))
+
--- day_2.rkt
+++ day_2.rkt
@@ -1,22 +1,16 @@
 #lang racket
 
 (require "common.rkt")
-(define input (pair-up (open-day 2) 2))
+(define input (pair-up (open-day 2)))
 
-(define (decoder1 pair acc)
+(define (decoder pair acc)
   (+. acc (match pair
-        [(list 'forward val) `(,val . 0)]
-        [(list 'down val)    `(0 . ,val)]
-        [(list 'up val)      `(0 . ,(- val))])))
+            [(list 'forward val) `(0 ,val  . ,(* val (car acc)))]
+            [(list 'down val)    `(,val 0 . 0)]
+            [(list 'up val)      `(,(- val) 0 . 0)])))
 
-(define (decoder2 pair acc)
-  (+. acc (match pair
-        [(list 'forward val) `(,val ,(* val (cddr acc)) . 0)]
-        [(list 'down val)    `(0 0 . ,val)]
-        [(list 'up val)      `(0 0 . ,(- val))])))
+(define (solver input)
+  (foldl decoder '(0 0 . 0) input))
 
-(define (solver decoder start)
-  (mult2 (foldl decoder start input)))
-
-(submit 1 (solver decoder1 '(0 . 0)) #f)
-(submit 2 (solver decoder2 '(0 0 . 0)) #f)
+(submit 1 (mult2 (solver input)) #f)
+(submit 2 (mult2 (cdr (solver input))) #f)
