d6061b1cdee3a592a9199b43aa65ef343e925279
Tevqoon <jure.smolar@gmail.com>
2021-12-03 16:36:18 +0100

    yes

--- common.rkt
+++ common.rkt
@@ -4,13 +4,15 @@
 
 ;; Administration
 
-(define (open-day n)
+(define (open-day n [lines? #f])
   "Opens the n-th day's input file as a list.
    If it doesn't exist, pull it with the aoc package."
   (let [(day-filename (format "day_~a.in" n))]
     (unless (file-exists? day-filename)
       (system (format "raco aoc -d ~a > ~a" n day-filename)))
-    (file->list day-filename)))
+    (if lines?
+        (file->lines day-filename)
+        (file->list day-filename))))
 
 (define (submit n val [submit? #f])
   (if submit?
@@ -44,29 +46,30 @@
 
 ;; Tuples are of form (x1 x2 . x3).
 
-(define (tuple. proc . tuples)
+(define (map. proc . tuples)
   "Applies proc to tuples elementwise.
-   Assumes same length tuples and at least one given element."
+   Assumes same length tuples and at least one given element. Also works on lists."
   (define (aux tuples)
-    (if (pair? (car tuples))
-        (cons (apply proc (map car tuples))
-              (aux (map cdr tuples)))
-        (apply proc tuples)))
+    (cond [(pair? (car tuples))
+           (cons (apply proc (map car tuples))
+              (aux (map cdr tuples)))]
+          [(null? (car tuples)) null]
+          [else (apply proc tuples)]))
   (aux tuples))
 
 ;; Some basic tuple operations
 
 (define (+. . tuples)
-  (apply tuple. + tuples))
+  (apply map. + tuples))
 
 (define (-. . tuples)
-  (apply tuple. - tuples))
+  (apply map. - tuples))
 
 (define (*. . tuples)
-  (apply tuple. * tuples))
+  (apply map. * tuples))
 
 (define (/. . tuples)
-  (apply tuple. / tuples))
+  (apply map. / tuples))
 
 (define (mult2 tuple)
   "Multiply first two elements of a tuple"
@@ -75,11 +78,27 @@
       (* (car tuple) (cdr tuple))))
 
 (define (tapply procs . tuples)
-  "Applies procs to tuples componentwise. Assumes operations and vectors are all the same length"
+  "Applies procs to tuples componentwise. Assumes operations and tuples are all the same length"
   (if (pair? procs)
       (cons (apply (eval (car procs)) (map car tuples))
             (tapply (cdr procs) (map cdr tuples)))
       (apply (eval procs) (car tuples))))
+
+;; Binary bullshit
+
+(define (binstr->binlist binstr)
+  (map (cut - <> 48)
+       (map char->integer (string->list binstr))))
+
+(define (binlist->binstr binlist)
+  (list->string (map integer->char
+                     (map (cut + <> 48) binlist))))
+
+(define (binstr->integer binstr)
+  (string->number (format "#b~a" binstr)))
+
+(define (binlist->integer binlist)
+  (binstr->integer (binlist->binstr binlist)))
 
 (provide (all-defined-out))
 
