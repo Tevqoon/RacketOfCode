f131ad03de6e4b351063517c2766ccedecf5d061
Tevqoon <jure.smolar@gmail.com>
2021-12-06 10:49:30 +0100

    Further yassification efforts

--- day_6.rkt
+++ day_6.rkt
@@ -2,18 +2,21 @@
 
 (require "common.rkt")
 
-(define input (open-day 6))
+(define (convert fish)
+  (for/list ([i (range 9)]) (count (cut = i <>) fish)))
+
+(define input (convert (open-day 6)))
 
 (define (stepper fish days)
-  (let ([fishes (for/list ([i (range 9)]) (count (cut = i <>) fish))])
-    (define (aux hd tl n)
-      (if (zero? n)
-          (+ (apply + hd) (apply + tl))
-          (aux (append (cdr hd) (take tl 1))
-               (list (+ (car hd) (cadr tl)) (caddr tl) (car hd))
-               (sub1 n))))
-    (let-values ([(hd tl) (split-at fishes 6)])
-      (aux hd tl days))))
+  (define (aux hd tl n)
+    (if (zero? n)
+        (+ (apply + hd) (apply + tl))
+        (aux (append (cdr hd) (take tl 1))
+             (list (+ (car hd) (cadr tl)) (caddr tl) (car hd))
+             (sub1 n))))
+    (let-values ([(hd tl) (split-at fish 6)])
+      (aux hd tl days)))
+
 
 (submit 1 (stepper input 80) #f)
 (submit 2 (stepper input 256) #f)
