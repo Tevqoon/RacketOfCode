7fc2ab94354273fe969fad79eb735ea54975fef8
Tevqoon <jure.smolar@gmail.com>
2021-12-07 14:04:16 +0100

    As much yass as i'm able to muster rn

--- day_7.rkt
+++ day_7.rkt
@@ -5,15 +5,9 @@
 
 (define input (open-day 7))
 
-(define (cost-1 pos crabs)
-  (foldl + 0 (map (λ(x) (abs (- x pos))) crabs)))
-
-(define (cost-2 pos crabs)
-  (foldl + 0 (map (λ(x) (binomial (add1 (abs (- x pos))) 2)) crabs)))
-
 (define (best crabs cost)
   (apply min (for/list ([pos (inclusive-range 0 (apply max crabs))])
-               (cost pos crabs))))
+               (foldl + 0 (map (cut cost <> pos) crabs)))))
 
-(submit 1 (best input cost-1) #f)
-(submit 2 (best input cost-2) #f)
+(submit 1 (best input (λ(x pos) (abs (- x pos)))) #f)
+(submit 2 (best input (λ(x pos) (binomial (add1 (abs (- x pos))) 2))) #f)
