4329025660b79fab58f9bc2eb3364b2f081f03e0
Tevqoon <jure.smolar@gmail.com>
2021-12-10 14:31:05 +0100

    Marginal yassification

--- day_10.rkt
+++ day_10.rkt
@@ -3,10 +3,8 @@
 (require "common.rkt")
 
 (define input (map string->list (open-day 10 #t)))
-
-(define opens '((#\( #\)) (#\[ #\]) (#\{ #\}) (#\< #\>)))
-(define error-scores '((#\) 3) (#\] 57) (#\} 1197) (#\> 25137)))
-(define completion-scores '((#\) 1) (#\] 2) (#\} 3) (#\> 4)))
+(define opens  '((#\( #\)) (#\[ #\]) (#\{ #\}) (#\< #\>)))
+(define scores '((#\) 3 1) (#\] 57 2) (#\} 1197 3) (#\> 25137 4)))
 
 (define (checker line)
   (define (aux line stack)
@@ -15,16 +13,15 @@
                    (aux xs (cons (cadr (assoc x opens)) stack))]
       [(cons x xs) #:when (equal? x (car stack))
                    (aux xs (cdr stack))]
-      [(cons x xs) (cadr (assoc x error-scores))]
+      [(cons x xs) (cadr (assoc x scores))]
       [else stack]))
   (aux line '()))
 
 (define (completion-score rest-line)
-  (foldl (λ(char score) (+ (* 5 score) (cadr (assoc char completion-scores)))) 0 rest-line))
+  (foldl (λ(char score) (+ (* 5 score) (caddr (assoc char scores)))) 0 rest-line))
 
 (define-values (corrupted incompletes) (partition number? (map checker input)))
 
 (submit 1 (apply + corrupted) #f)
-
 (submit 2 (list-ref (sort (map completion-score incompletes) <)
                     (quotient (length incompletes) 2)) #f)
