df223d4d870aa0ca08cdcc44fe8db50acc8dc24b
Tevqoon <jure.smolar@gmail.com>
2021-12-12 14:50:17 +0100

    Slightly washed but still dirty

--- day_12.rkt
+++ day_12.rkt
@@ -14,36 +14,25 @@
 
 (define input (collect (map (cut string-split <> "-") (open-day 12 #t))))
 
-(define (explore1 caves)
+(define (explore caves disallow-duplicate?)
   (define (aux acc ongoing)
     (match ongoing
       [(cons x xs) #:when (equal? (car x) "end")
                    (aux (add1 acc) xs)]
-      [(cons x xs) (aux acc
-                        (append (map (cut cons <> x)
-                                     (set-subtract (hash-ref caves (car x))
-                                                   (filter downcase? x)))
-                                xs))]
+      [(cons x xs)
+       (aux acc
+            (append (map (cut cons <> x)
+                         (if (or disallow-duplicate? ; If duplicates aren't allowed, just don't care
+                                 (check-duplicates (filter downcase? x)))
+                             ;if there's already two small caves, filter them aggressively
+                             (set-subtract (hash-ref caves (car x))
+                                           (filter downcase? x))
+                             ;othwerise just take all small caves as options except "start"
+                             (set-subtract (hash-ref caves (car x))
+                                           (list "start"))))
+                    xs))]
       [empty acc]))
   (aux 0 '(("start"))))
 
-(define (explore2 caves)
-  (define (aux acc ongoing)
-    (match ongoing
-      [(cons x xs) #:when (equal? (car x) "end")
-                   (aux (add1 acc) xs)]
-      [(cons x xs) (aux acc
-                        (append (map (cut cons <> x)
-                                     (if (check-duplicates (filter downcase? x))
-                                         ;if there's already two small caves, filter them aggressively
-                                         (set-subtract (hash-ref caves (car x))
-                                                       (filter downcase? x))
-                                         ;othwerise just take all small caves as options except "start"
-                                         (set-subtract (hash-ref caves (car x))
-                                                       (list "start"))))
-                                xs))]
-      [empty acc]))
-  (aux 0 '(("start"))))
-
-(submit 1 (explore1 input) #f)
-(submit 2 (explore2 input) #f)
+(submit 1 (explore input #t) #f)
+(submit 2 (explore input #f) #f)
