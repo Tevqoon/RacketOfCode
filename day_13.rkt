61ad26c9f8fc3838a5856b164ca5739beb2ad6ce
Tevqoon <jure.smolar@gmail.com>
2021-12-13 23:00:36 +0100

    I have ascended beyond the mortal plane

--- day_13.rkt
+++ day_13.rkt
@@ -4,21 +4,19 @@
 
 (define-values (numbers folds)
   (let*-values ([(numbers folds) (splitf-at (open-day 13) number?)])
-    (values (list->set (pair-up numbers))
+    (values (let*-values ([(numbers) (list->set (pair-up numbers))]
+                         [(x0 y0) (apply values (map (λ(x) (+ 1 x (modulo x 2))) (apply map max (set->list numbers))))])
+              (build-list y0 (λ(y) (build-list x0 (λ(x) (set-member? numbers (list x y)))))))  
             (map (λ(x) (list (match (car x) ["x" #t] ["y" #f]) (string->number (cadr x))))
                  (map (compose (curryr string-split "=") symbol->string)
                       (cddr (remove-duplicates folds)))))))
-
-(define (make-paper numbers)
-  (let-values ([(x0 y0) (apply values (map (λ(x) (+ 1 x (modulo x 2))) (apply map max (set->list numbers))))])
-    (build-list y0 (λ(y) (build-list x0 (λ(x) (set-member? numbers (list x y))))))))
 
 (define (fold instr paper)
   (let-values ([(top bot) (split-at (if (car instr) (transpose paper) paper) (cadr instr))])
     ((λ(x) (if (car instr) (transpose x) x)) (map (curry map (cut or <> <>)) top (reverse (cdr bot))))))
 
 (define (printer paper)
-  (for ([line paper]) (for-each (λ(c) (display (if c "o" " "))) line) (displayln "")))
+  (for ([line paper]) (for-each (λ(c) (display (if c "█" " "))) line) (displayln "")))
 
-(submit 1 (count identity (flatten (fold (car folds) (make-paper numbers)))) #f)
-(printer (foldl fold (make-paper numbers) folds))
+(submit 1 (count identity (flatten (fold (car folds) numbers))) #f)
+(printer (foldl fold numbers folds))
